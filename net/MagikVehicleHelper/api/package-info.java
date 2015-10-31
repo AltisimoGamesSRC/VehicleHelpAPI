dac9f9d638411fe32a70cfa33884a8cde19be05e
T0psid3xDev <altisimomods@gmail.com>
2015-10-30 21:49:25 -0700

    Initial Commit

---
+++ net/MagikVehicleHelper/api/package-info.java
@@ -0,0 +1,23 @@
+@API(owner = "MagikVehicleHelperAPI", apiVersion = "1.0.0", provides = "Vehicle|API")
+package net.MagikVehicleHelper.api;
+
+import cpw.mods.fml.common.API;
+
+/**
+ * This API is Made by Magik, If you did not get this without my written permission, please remove this immediantely, as this is
+ * to only be used if I  Have Given Permission to do so. Chances Are If you do have this you know me so you can go ahead and
+ * delete this if you would like. I do not intend to release this to the republic and is for friends only.
+ * 
+ * (This Version Of The API Is For the JurassiCraft 2, SuperNaturalCraft, or Steven Universe Mod ONLY)
+ * 
+ * API = Magik's Vehicle Helper API
+ * 
+ * Version = 1.0
+ * 
+ * 
+ * 
+ * Copyright (C) Magik 2014
+ * 
+ * 
+ * @author Magik
+ */
---
+++ net/MagikVehicleHelper/api/packet/PacketHandler.java
@@ -0,0 +1,29 @@
+package net.MagikVehicleHelper.api.packet;
+
+import java.io.IOException;
+
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.network.FMLNetworkEvent.ClientCustomPacketEvent;
+import cpw.mods.fml.common.network.FMLNetworkEvent.ServerCustomPacketEvent;
+
+public class PacketHandler {
+protected String channelName = "MagikVehicleHelperAPI";
+	
+	@SubscribeEvent
+	public void onServerPacket(ServerCustomPacketEvent event) throws IOException
+	{
+
+		
+		
+		
+	}
+
+	@SubscribeEvent
+	public void onClientPacket(ClientCustomPacketEvent event) throws IOException
+	{	
+		
+		
+		
+		
+	}
+}
